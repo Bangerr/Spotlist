3a3b9a3a64e98aa6c9854c9eea2e22ae84f60655
Bangerr <devilism09@gmail.com>
2025-03-08 16:10:39 +0100

    Implement: Viberchecker and TrackDetails

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,28 @@
 "use client";
 import { useSession } from "next-auth/react";
 import Login from "./login/page";
+import { ChartConfig } from "@/components/ui/chart";
+import VibeChecker from "@/components/VibeChecker";
+import TrackDetails from "@/components/TrackDetails";
+
+const chartConfig = {
+  desktop: {
+    label: "Desktop",
+    color: "#2563eb",
+  },
+  mobile: {
+    label: "Mobile",
+    color: "#60a5fa",
+  },
+} satisfies ChartConfig;
+
+const chartData = [
+  { vibe: "Danceability", value: 85 },
+  { vibe: "Energy", value: 90 },
+  { vibe: "Speechiness", value: 35 },
+  { vibe: "Acousticness", value: -15.0 },
+  { vibe: "Valence", value: 70 },
+];
 
 export default function Home() {
   const session = useSession();
@@ -12,8 +34,11 @@ export default function Home() {
 
   return (
     <main className="w-full px-10 md:w-[30%] mx-auto h-full">
-      {/* Playlists */}
-      <div></div>
+      {/* Vibe checker */}
+      <div className="mt-20">
+        <VibeChecker chartData={chartData} />
+        <TrackDetails />
+      </div>
     </main>
   );
 }
---
+++ src/components/TrackDetails.tsx
@@ -0,0 +1,98 @@
+"use client";
+import React, { useEffect, useState } from "react";
+
+interface TrackName_Artist {
+  artist: string;
+  name: string;
+}
+
+interface TrackDetails {
+  track: TrackName_Artist[];
+}
+
+const TrackDetails = () => {
+  const [trackData, setTrackData] = useState({});
+  const [isrc_ids, setIsrc_ids] = useState<string[]>([]);
+  const [track_lyrics, setTrack_lyrics] = useState<string[]>([]);
+  const trackId = "2plbrEY59IikOBgBGLjaoe?si=9962ca09330f464a";
+  const [title, setTitle] = useState<string[]>([]);
+  const [artist, setArtist] = useState<string[]>([]);
+
+  useEffect(() => {
+    async function fetchTrack() {
+      try {
+        const response = await fetch("/api/track", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ trackId }),
+        });
+
+        if (response.ok) {
+          const data = await response.json();
+          console.log(data);
+          setTrackData(data);
+
+          const { external_ids } = data;
+          setIsrc_ids((prevIsrcIds) => [...prevIsrcIds, external_ids.isrc]);
+          setTitle((prevState) => [...prevState, data.name]);
+          setArtist((prevState) => [...prevState, data.artists[0].name]);
+        } else {
+          throw new Error("Failed to fetch the Track.");
+        }
+      } catch (err) {
+        console.error("Playlist fetching failed Step: ", err);
+      }
+    }
+
+    fetchTrack();
+  }, []);
+
+  useEffect(() => {
+    async function fetchLyrics() {
+      try {
+        const response = await fetch("/api/lyricsOvh", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ artist, title }),
+        });
+
+        if (response.ok) {
+          const data = await response.json();
+          console.log(data);
+
+          //musixwatch lyrics
+          // const { lyrics } = data.message.body;
+          // console.log("Lyrics: ", lyrics.lyrics_body);
+          setTrack_lyrics(data.lyrics);
+        } else {
+          throw new Error("Failed to fetch the Lyrics.");
+        }
+      } catch (err) {
+        console.error("Playlist fetching failed Step: ", err);
+      }
+    }
+    fetchLyrics();
+  }, [title]);
+
+  return (
+    <div>
+      {isrc_ids ? (
+        <div className="flex flex-col">
+          <div>ISRC: {isrc_ids[0]}</div>
+
+          <div>Title: {title}</div>
+          <div>Artist: {artist}</div>
+          <div>Lyrics: {track_lyrics}</div>
+        </div>
+      ) : (
+        <div>Loading external IDS...</div>
+      )}
+    </div>
+  );
+};
+
+export default TrackDetails;
