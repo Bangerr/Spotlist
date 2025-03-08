b5d6be986fd5d53bba17604e7d19b1d279051baa
Bangerr <devilism09@gmail.com>
2025-03-08 15:39:15 +0100

    renamed api enpoints

--- src/app/track/page.tsx
+++ src/app/track/page.tsx
@@ -1,14 +1,15 @@
 "use client";
 import React, { useEffect, useState } from "react";
 
-export default function Playlist() {
-  const [trackData, setTrackData] = useState();
+export default function Track() {
+  const [trackData, setTrackData] = useState({});
+  const [isrc_ids, setIsrc_ids] = useState<string[]>([]);
   const trackId = "7ycWLEP1GsNjVvcjawXz3z?si=23904013924b4668";
 
   useEffect(() => {
     async function fetchPlaylist() {
       try {
-        const response = await fetch("/api/playlists", {
+        const response = await fetch("/api/track", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
@@ -18,10 +19,14 @@ export default function Playlist() {
 
         if (response.ok) {
           const data = await response.json();
+
           console.log(data);
           setTrackData(data);
+          const { external_ids } = data;
+          setIsrc_ids((prevIsrcIds) => [...prevIsrcIds, external_ids.isrc]);
+          console.log(isrc_ids);
         } else {
-          throw new Error("Failed to fetch the Playlist.");
+          throw new Error("Failed to fetch the Track.");
         }
       } catch (err) {
         console.error("Playlist fetching failed Step: ", err);
@@ -32,10 +37,11 @@ export default function Playlist() {
 
   return (
     <div className="w-full px-10 md:w-[30%] mx-auto mt-16">
-      <div>{/* <p>Album Name: {trackData.artists[0].name}</p> */}</div>
-
-      {/* Track Card */}
-      <div>fe</div>
+      {isrc_ids ? (
+        <div>ISRC: {isrc_ids}</div>
+      ) : (
+        <div>Loading external IDS...</div>
+      )}
     </div>
   );
 }
