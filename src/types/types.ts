729ea4a24bc6a19b3529e0cd0197a5b79fa4d9ba
Bangerr <devilism09@gmail.com>
2025-03-09 17:32:43 +0100

    update types

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,18 +1,3 @@
-import { DefaultSession } from "next-auth";
-
-interface MyUser {
-  name?: string | null;
-  email?: string | null;
-  picture?: string | null;
-  image?: string | null;
-  accessToken?: string | null;
-}
-
-export interface MySession extends Omit<DefaultSession, "user"> {
-  user?: MyUser;
-  expires: string;
-}
-
 interface Image {
   height: number | null;
   url: string | null;
@@ -22,7 +7,7 @@ interface Image {
 export interface Album {
   id: string;
   name: string;
-  artists: [Artist];
+  artists: [Artist]; // tuple with exactly one lement
   images?: [Image];
   album_type?: string;
   release_date?: string;
@@ -40,6 +25,7 @@ export interface Artist {
     total: number;
   };
   genres?: [string];
+  external_urls: { spotify: string };
 }
 
 export interface Track {
@@ -48,7 +34,7 @@ export interface Track {
   album: Album;
   artists: [Artist];
   duration_ms: number;
-  preview_url: string;
+  external_urls: { spotify: string };
 }
 
 export interface PlaylistType {
@@ -70,19 +56,4 @@ export interface PlaylistType {
   };
   type?: string;
   total?: number;
-}
-
-export interface SearchResults {
-  albums?: {
-    items: Album[];
-  };
-  artists?: {
-    items: Artist[];
-  };
-  playlists?: {
-    items: PlaylistType[];
-  };
-  tracks?: {
-    items: Track[];
-  };
 }
