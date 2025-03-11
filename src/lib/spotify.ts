c91aa26acf04862c7caaf07498e70f8e488d930f
Bangerr <devilism09@gmail.com>
2025-03-11 20:27:40 +0100

    Fix(deployment): resolve following issues
    - hot fix: verify the response exists before calling json()
    - fixup the types
    - retrieve access_token correrctly from the session

--- src/lib/spotify.ts
+++ src/lib/spotify.ts
@@ -1,3 +1,4 @@
+import { AuthUser } from "@/app/api/auth/[...nextauth]/route";
 import { authOptions } from "@/config/auth";
 import { getServerSession } from "next-auth";
 
@@ -24,7 +25,13 @@ import { getServerSession } from "next-auth";
 export const getPlaylist = async (playlist_id: string) => {
   const session = await getServerSession(authOptions);
 
-  const access_token = session?.token?.access_token;
+  if (!session) {
+    return console.error("Session not found or user not signed in.");
+  }
+
+  const user = session.user as AuthUser;
+
+  const access_token = user.access_token;
 
   return fetch(`https://api.spotify.com/v1/playlists/${playlist_id}`, {
     headers: {
@@ -40,8 +47,12 @@ export const getTrack = async (trackId: string) => {
     return console.error("Session not found or user not signed in.");
   }
 
-  const access_token = session?.token?.access_token;
-  const expires_at = session?.token?.expires_at;
+  const user = session.user as AuthUser;
+  console.log("User: ", user);
+
+  const access_token = user.access_token;
+  console.log("Access Token: ", access_token);
+  const expires_at = user.expires_at;
   const readableExpiry = new Date(expires_at * 1000).toLocaleString();
   console.log("Readable: ", readableExpiry);
 
@@ -59,8 +70,10 @@ export const getTopTracks = async () => {
     return console.error("Session not found or user not signed in.");
   }
 
-  const access_token = session?.token?.access_token;
-  const expires_at = session?.token?.expires_at;
+  const user = session.user as AuthUser;
+
+  const access_token = user.access_token;
+  const expires_at = user.expires_at;
   const readableExpiry = new Date(expires_at * 1000).toLocaleString();
   console.log("Readable: ", readableExpiry);
 
@@ -83,8 +96,10 @@ export const getTopArtists = async () => {
     return console.error("Session not found or user not signed in.");
   }
 
-  const access_token = session?.token?.access_token;
-  const expires_at = session?.token?.expires_at;
+  const user = session.user as AuthUser;
+
+  const access_token = user.access_token;
+  const expires_at = user.expires_at;
   const readableExpiry = new Date(expires_at * 1000).toLocaleString();
   console.log("Readable: ", readableExpiry);
 
@@ -107,7 +122,9 @@ export const customGet = async (url: string) => {
     return console.error("Session not found or user not signed in.");
   }
 
-  const access_token = session?.token?.access_token;
+  const user = session.user as AuthUser;
+
+  const access_token = user.access_token;
 
   return fetch(url, {
     headers: {
