973a745c4062a5f1f298ffc58422f449879e2df7
Bangerr <devilism09@gmail.com>
2025-04-08 19:34:53 +0200

    style: update UI with new design and styling

--- src/components/TopItems.tsx
+++ src/components/TopItems.tsx
@@ -67,71 +67,58 @@ const TopItems = () => {
         </p>
       </div>
       {/* List: Top Tracks and Top Artists */}
-      <div className="bg-secondary border border-black dark:border-primary rounded-3xl ">
-        <table className="min-w-full">
-          <thead className="border-b border-b-black dark:border-primary">
-            <tr>
-              <th className="">
-                <p className="px-2 py-8 ml-16 text-left text-lg font-semibold text-gray-700 dark:text-[#d6cfcf]">
-                  Top Tracks
-                </p>
-              </th>
-              <th className="">
-                <p className="px-2 py-8 ml-16 text-left text-lg font-semibold text-gray-700 dark:text-[#d6cfcf]">
-                  Top Artists
-                </p>
-              </th>
-            </tr>
-          </thead>
-
-          <tbody className="w-full p-10">
-            {/* Top Tracks */}
+      <div className="flex md:flex-row flex-col gap-5">
+        {/* Top Tracks */}
+        <div className="basis-1/2 bg-secondary/50 backdrop-blur-sm rounded-2xl p-6 border border-gray-200 dark:border-gray-700">
+          <h2 className="text-2xl text-gray-800 dark:text-gray-500 font-bold mb-6">
+            Top Tracks
+          </h2>
+          <div className="flex flex-col gap-5">
             {topTracks?.map((track, index) => (
-              <tr key={index} className="">
-                <td className="w-1/2 md:px-10 px-2 py-3 text-[#424242] dark:text-[#a19a9a] font-medium hover:text-[#a19a9a] dark:hover:text-[#6c6b6b]">
-                  <a
-                    key={index}
-                    className="flex flex-row gap-3 items-center"
-                    href={track.external_urls.spotify}
-                    target="_blank">
-                    <span>{index + 1}. </span>
+              <a
+                key={index}
+                className="flex flex-row gap-3 items-center"
+                href={track.external_urls.spotify}
+                target="_blank">
+                <span className="text-gray-500">{index + 1}. </span>
 
-                    <Image
-                      src={track.album.images[0].url}
-                      alt="profilePicture"
-                      width={50}
-                      height={50}
-                      className="border rounded-full"
-                    />
-                    {track.name}
-                  </a>
-                </td>
-              </tr>
+                <Image
+                  src={track.album.images[0].url}
+                  alt="profilePicture"
+                  width={50}
+                  height={50}
+                  className="border rounded-full"
+                />
+                {track.name}
+              </a>
             ))}
-            {/* Top Artists */}
+          </div>
+        </div>
+        {/* Top Artists */}
+        <div className="basis-1/2 bg-secondary/50 backdrop-blur-sm rounded-2xl p-6 border border-gray-200 dark:border-gray-700">
+          <h2 className="text-2xl text-gray-800 dark:text-gray-500 font-bold mb-6">
+            Top Artits
+          </h2>
+          <div className="flex flex-col gap-5">
             {topArtists?.map((artist, index) => (
-              <tr key={index} className="">
-                <td className="w-1/2 md:px-10 px-2 py-3 text-[#424242] dark:text-[#a19a9a] font-medium hover:text-[#a19a9a] dark:hover:text-[#6c6b6b]">
-                  <a
-                    key={index}
-                    className="flex flex-row gap-3 items-center"
-                    href={artist.external_urls.spotify}
-                    target="_blank">
-                    <span>{index + 1}. </span>
-                    <Image
-                      src={artist.images[0].url}
-                      alt="profilePicture"
-                      width={50}
-                      height={10}
-                      className="border rounded-full"
-                    />
-                    {artist.name}
-                  </a>
-                </td>
-              </tr>
+              <a
+                key={index}
+                className="flex flex-row gap-3 items-center"
+                href={artist.external_urls.spotify}
+                target="_blank">
+                <span className="text-gray-500">{index + 1}. </span>
+                <Image
+                  src={artist.images[0].url}
+                  alt="profilePicture"
+                  width={50}
+                  height={10}
+                  className="border rounded-full"
+                />
+                {artist.name}
+              </a>
             ))}
-          </tbody>
-        </table>
+          </div>
+        </div>
       </div>
     </div>
   );
