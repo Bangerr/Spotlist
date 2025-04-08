6f0402b30c29ac6b8e244e1cc38317fdbd053885
Bangerr <devilism09@gmail.com>
2025-04-08 19:34:13 +0200

    remove width

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,7 +8,7 @@ const Footer = () => {
       id="footer"
       className="w-full px-10 lg:w-[50%] md:w-[75%] mx-auto py-16">
       <div className="mx-auto md:gap-16 gap-4 flex md:flex-row flex-col items-center justify-content">
-        <div className="w-3/12 mt-0 flex items-center justify-center ">
+        <div className="mt-0 flex items-center justify-center ">
           <p className="text-sm min-w-max">© Designed by Sukh</p>
         </div>
         <div className=" w-full flex flex-row gap-3 items-center md:justify-end justify-center">
